b8f8ea023554aca53d6c64fa03aaab183b8ee194
Bartłomiej Szkodny <szkobart@outlook.com>
2024-09-11 23:44:58 +0200

    commit

--- app/lib/action.tsx
+++ app/lib/action.tsx
@@ -1,55 +1,45 @@
 "use server";
-import { User} from "./models";
+import { User } from "./models";
 import { connectToDatabase } from "./utils";
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
-export const RegisterUser = async (formData: any) => {
-    
+export const RegisterUser = async (formData:any) => {
+    const { username, email, password, repeat } = formData;
+    console.log(username, email, password, repeat);
 
-    const Username = formData.username;
-    const Email = formData.email;
-    const Password = formData.password;
-    const Repeatpassword = formData.repeat
+    try {
+        // Ensure the database connection is established
+        await connectToDatabase();
 
+        // Check if passwords match
+        if (password !== repeat) {
+            console.log('Passwords do not match');
+            return 'Passwords do not match';
+        }
 
-try {
-    
-    connectToDatabase();
-    const newUser = new User({
-        username: Username,
-        email: Email,
-        password:Password,
-        isAdmin: false
-    })
-
-    
-    if (Password !== Repeatpassword) {
-        console.log('Passwords do not match');
-        return 'Passwords do not match';
-
-
-    }
-    else {
+        // Password validation
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // Example: Minimum eight characters, at least one letter and one number
-        if (!Password.match(passwordRegex)) {
+        if (!password.match(passwordRegex)) {
             console.log('Password must be at least 8 characters long, and contain at least one letter and one number');
             return 'Password must be at least 8 characters long, and contain at least one letter and one number';
         }
-        else {
-            await newUser.save();
-        }
+
+        // Create a plain object with the user data to save
+        const newUser = {
+            username: username,
+            email: email,
+            password: password,
+            isAdmin: false
+        };
+
+        // Save the user to the database
+        const userDocument = new User(newUser);
+        await userDocument.save();
+
+        // Redirect after successful registration
+        redirect('http://localhost:3000/login');
+    } catch (error) {
+        console.log('Error registering user:');
+        return 'Error registering user';
     }
-
-
-} catch (error) {
-    console.log(error);
-    return error;
-    
-}
-
-redirect('http://localhost:3000/login');
-
-
-    
-
-}
+};
