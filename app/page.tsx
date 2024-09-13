f0d1a85e1d1faaed789a3a9ab5d0a1ebec7fe049
Bartłomiej Szkodny <szkobart@outlook.com>
2024-09-13 23:23:57 +0200

    login page changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { getServerAuthSession } from "@/server/auth";
+
 export default function Home() {
-  const authSession = await getServerAuthSession()
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <img src="https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_256.gif"/>
